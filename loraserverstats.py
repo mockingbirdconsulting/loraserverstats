e4088bf060b7de6f8942d3200ef4bcc49c5c0322
Matthew Macdonald-Wallace <matt@mockingbirdconsulting.co.uk>
2018-11-19 13:41:57 +0000

    Iterate over the stats and print them to the command line

--- loraserverstats.py
+++ loraserverstats.py
@@ -9,17 +9,26 @@ cfg = {}
 with open(cfg_file, "r") as ymlfile:
     cfg = yaml.load(ymlfile)
 
-auth_data = {
-        "username": cfg['server']['auth']['username'],
-        "password": cfg['server']['auth']['password']
-        }
-
 auth_request = requests.post(
         cfg['server']['uri']+"/api/internal/login",
-        json = auth_data,
-        verify = False
+        json = cfg['server']['auth'],
+        verify = certifi.where()
         )
 
 auth_tok = auth_request.json()
 
-print(json.dumps(auth_tok))
+jwt = auth_tok['jwt']
+
+auth_header = {"Grpc-Metadata-Authorization": jwt}
+
+stats = {}
+for qtype in ['applications','gateways', 'devices']:
+    stats[qtype] = requests.get(
+            cfg['server']['uri']+"/api/%s" % qtype,
+            headers = auth_header,
+            verify = certifi.where()
+            ).json()
+
+
+
+print(stats)
