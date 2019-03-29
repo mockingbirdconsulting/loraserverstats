f03c25750150136cefd7693385b65d842a4e88f1
Matthew Macdonald-Wallace <matt@mockingbirdconsulting.co.uk>
2019-03-29 06:50:40 +0000

    Update to make the global stats easier to read

--- loraserverstats.py
+++ loraserverstats.py
@@ -11,24 +11,23 @@ with open(cfg_file, "r") as ymlfile:
 
 auth_request = requests.post(
         cfg['server']['uri']+"/api/internal/login",
-        json = cfg['server']['auth'],
-        verify = certifi.where()
+        json = cfg['server']['auth']
         )
 
 auth_tok = auth_request.json()
 
 jwt = auth_tok['jwt']
 
+#print(jwt)
+
 auth_header = {"Grpc-Metadata-Authorization": jwt}
 
 stats = {}
-for qtype in ['applications','gateways', 'devices']:
+for qtype in ['organizations','applications','gateways', 'devices']:
     stats[qtype] = requests.get(
             cfg['server']['uri']+"/api/%s" % qtype,
-            headers = auth_header,
-            verify = certifi.where()
-            ).json()
-
+            headers = auth_header
+            ).json()['totalCount']
 
 
 print(stats)
